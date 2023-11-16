289acf3220ec1e413423083e8f940d369e4f8d09
David Serrano <franklinserrano23@gmail.com>
2023-11-16 16:40:51 -0500

    Actualización del archivo 'setup.py'

--- setup.py
+++ setup.py
@@ -6,7 +6,66 @@ setup(
     author='SurivZ',
     author_email='franklinserrano23@email.com',
     description='Paquete para manejar conexiones de bases de datos SQLite3 y operaciones CRUD.',
-    long_description=open('readme.md', encoding='utf-8').read(),
+    long_description="""# Py-SQLite-DB-Connection
+
+Este paquete proporciona una clase `Connect` para gestionar conexiones y operaciones CRUD en bases de datos SQLite3 de manera sencilla y estandarizada.
+
+## Instalación
+
+Puedes instalar el paquete utilizando pip:
+```nash
+pip install PySQLiteDBConnection
+```
+
+## Ejemplo de uso
+
+### Importar el paquete
+```Python
+from PySQLiteDBConnection import Connect
+```
+
+### Crear una instancia de la clase Connect
+```Python
+db = Connect('path_to_your_database.db')
+```
+
+### Establecer conexión a la base de datos
+```Python
+connected = db.connect()
+```
+
+### Leer todos los registros de una tabla
+```Python
+table_data = db.read_table('table_name')
+```
+
+### Insertar datos en una tabla
+```Python
+data_to_insert = {'column1': 'value1', 'column2': 'value2'}
+inserted = db.insert_into_table('table_name', data_to_insert)
+```
+
+### Actualizar registros en una tabla
+```Python
+update_data = {'column1': 'new_value'}
+condition = {'column2': 'value2'}
+updated = db.update_record('table_name', update_data, condition)
+```
+
+### Eliminar registros de una tabla
+```Python
+delete_condition = {'column1': 'value1'}
+deleted = db.delete_record('table_name', delete_condition)
+```
+
+### Cerrar la conexión con la base de datos
+```Python
+db.close()
+```
+
+
+## Contribución
+  ¡Las contribuciones son bienvenidas! Si encuentras algún error o tienes sugerencias para mejoras, siéntete libre de abrir un issue o enviar un pull request.""",
     long_description_content_type='text/markdown',
     url='https://github.com/SurivZ/Py-SQLite-DB-Connection',
     packages=find_packages(),
